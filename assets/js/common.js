1e6aacd3a4bd72921874fb2eea608c7e3a1a8c09
KyleYoung <kyleyoung_ymj@163.com>
2024-03-09 17:35:15 +0800

    add social icons to navbar, add TLDR and abstract to ICDT 2024 paper

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,11 +2,18 @@ $(document).ready(function () {
   // add toggle functionality to abstract and bibtex buttons
   $("a.abstract").click(function () {
     $(this).parent().parent().find(".abstract.hidden").toggleClass("open");
-    $(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open");
+    // $(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open");
+    // $(this).parent().parent().find(".tldr.hidden.open").toggleClass("open");
   });
   $("a.bibtex").click(function () {
+    // $(this).parent().parent().find(".abstract.hidden.open").toggleClass("open");
     $(this).parent().parent().find(".bibtex.hidden").toggleClass("open");
-    $(this).parent().parent().find(".abstract.hidden.open").toggleClass("open");
+    // $(this).parent().parent().find(".tldr.hidden.open").toggleClass("open");
+  });
+  $("a.tldr").click(function () {
+    // $(this).parent().parent().find(".abstract.hidden.open").toggleClass("open");
+    // $(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open");
+    $(this).parent().parent().find(".tldr.hidden").toggleClass("open");
   });
   $("a").removeClass("waves-effect waves-light");
 
